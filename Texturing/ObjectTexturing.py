d428c6b5e64be9257bd204a6d7d8642ff0320a9f
SadSchelz <67759165+SadSchelz@users.noreply.github.com>
2021-07-28 14:09:22 -0700

    Update ObjectTexturing.py

--- Texturing/ObjectTexturing.py
+++ Texturing/ObjectTexturing.py
@@ -2,18 +2,13 @@ import sys, os
 from panda3d.core import *
 from direct.showbase.ShowBase import ShowBase, Loader
 from direct.task import Task
-from direct.actor.Actor import Actor
-from direct.gui.OnscreenText import OnscreenText
-from direct.gui.DirectGui import *
-from direct.stdpy import threading
-from direct.interval.IntervalGlobal import Sequence, Func, Wait
 
 class ModelTexture(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         self.RelativPath	  = Filename.fromOsSpecific(os.path.abspath(sys.path[0])).getFullpath()     ## Relative Location  
         self.WallObj          = self.loader.loadModel(self.RelativPath + '/BasicWall.egg')              ## Model Object
-        self.Texture1         = self.loader.loadTexture(self.RelativPath + '/images.jpg')               ## Texture File
+        self.Texture1         = self.loader.loadTexture(self.RelativPath + '/BrickTexture.jpg')               ## Texture File
         self.ts               = TextureStage('ts')                                      
 
         self.WallObj.setPos(0, 25, 0)
